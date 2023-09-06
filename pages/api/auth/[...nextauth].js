2311e660ab200f6902dba8a096e14fa38b056eb0
Harshalak02 <karangaleharshal8@gmail.com>
2023-09-06 11:11:49 +0530

    Added one more admin

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,7 +3,7 @@ import { MongoDBAdapter } from '@next-auth/mongodb-adapter';
 import NextAuth, { getServerSession } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
-const adminEmails = ['karangaleharshal8@gmail.com', 'karangalesangita@gmail.com'];
+const adminEmails = ['karangaleharshal8@gmail.com', 'karangalesangita@gmail.com', 'demoralizer7@gmail.com'];
 
 export const authOptions = {
 
